b5563692e99c6b56289b552e18d3ee539e449449
xushiwei <x@goplus.org>
2025-05-02 21:15:11 +0800

    configEx.PkgName, Template.FromVer

--- getsentry/sentry-native/all/test_v1_package/conanfile.py
+++ getsentry/sentry-native/all/test_v1_package/conanfile.py
@@ -1,20 +1,25 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
+from conan.tools.build import cross_building
+from conan.tools.files import mkdir
+from conan.tools.scm import Version
 import os
 
 
-class TestPackageConan(ConanFile):
+class TestPackageV1Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "pkg_config"
-
-    def build_requirements(self):
-        self.build_requires("pkgconf/1.9.3")
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["HAS_PROXY"] = Version(self.deps_cpp_info["sentry-crashpad"].version) > "0.6.2"
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not cross_building(self):
+            test_env_dir = "test_env"
+            mkdir(self, test_env_dir)
             bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+            handler_exe = "crashpad_handler.exe" if self.settings.os == "Windows" else "crashpad_handler"
+            handler_bin_path = os.path.join(self.deps_cpp_info["sentry-crashpad"].rootpath, "bin", handler_exe)
+            self.run(f"{bin_path} {test_env_dir} {handler_bin_path}", run_environment=True)
