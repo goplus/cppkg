adbb9e1c448c46c76ddfab12b4578c9d97e3d0ab
xushiwei <x@goplus.org>
2025-05-04 18:28:07 +0800

    cmd/cppkg: use llgo/xtool/cppkg

--- cmd/cppkg/cppkg.go
+++ cmd/cppkg/cppkg.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+
+	"github.com/goplus/llgo/xtool/cppkg"
 )
 
 // cppkg 7bitcoder/7bitconf@1.2.0
@@ -13,32 +14,5 @@ func main() {
 		return
 	}
 
-	pkgPath, ver := parsePkgVer(os.Args[1])
-	if ver == "" {
-		panic("TODO: get latest version")
-	}
-
-	m, err := New("")
-	check(err)
-
-	const flags = IndexAutoUpdate | ToolQuietInstall
-	pkg, err := m.Lookup(pkgPath, ver, flags)
-	check(err)
-
-	err = m.Install(pkg, flags)
-	check(err)
-}
-
-func parsePkgVer(pkg string) (string, string) {
-	parts := strings.SplitN(pkg, "@", 2)
-	if len(parts) == 1 {
-		return parts[0], ""
-	}
-	return parts[0], parts[1]
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
+	cppkg.Install(os.Args[1], cppkg.DefaultFlags)
 }
